c942559fcfa19ca5b235a0f51d3f2b826950505f
Yousef-Essam <yousef.essam02@hotmail.com>
2022-10-06 15:17:20 +0200

    Pushing Changes

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,4 +1,4 @@
 eb init $EB_APP --region $AWS_DEFAULT_REGION --platform node.js && \
 eb use $EB_ENV && \
-eb setenv POSTGRES_USERNAME=full_stack_user POSTGRES_PASSWORD=qwerty123456 POSTGRES_HOST=database-1.c4bvywzfftc8.us-east-1.rds.amazonaws.com POSTGRES_DB=udagram_db JWT_SECRET=mysecretstring && \
+eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_HOST=$POSTGRES_HOST POSTGRES_DB=$POSTGRES_DB JWT_SECRET=$JWT_SECRET && \
 eb deploy
